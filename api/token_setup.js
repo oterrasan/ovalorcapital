de11d7e1f0271c1cd4ff7f0be7cc0c55cd7ea3da
oterrasan <roberto@oterrasan.com.br>
2026-04-19 15:17:00 -0300

    token_setup: buscar IG ID correto da pagina

--- api/token_setup.js
+++ api/token_setup.js
@@ -1,33 +1,12 @@
 export default async function handler(req, res) {
-  try {
-    const SHORT = "EAAubUN64VCABRFtsz3MVZC5jmDB0hzulNBgiUoTGtSPFjFIiXatsv5DmSsrdJEGy32YhjA4jjF3I1L0uBHMbzAyCyMjaQl1vas8TgOr6q2ODdJIjZAYbZBu95fcZCnZCLjZCe9z1V7GYCsGfBqxD8eYuBgShWZCiczZAKo53xZAm7vP7jmhIx4ELnYI0ninh8NSvE9joU2oR8F8iEcOkSxD2cdWtp2MXCREWkX9PhMEiEZBsmU5T2ZBcMtQYjSsbO5yAi5roRm6pc8EbY3PtYXKScHr7NTTZAtDsnb2ifWcZD";
-    const APP_ID = "3266996380128288";
-    const APP_SECRET = "6c594a465e2ad4ca75bab999fbfed524";
+  const TOKEN = "EAAubUN64VCABRKHaCJ1rH1wxZAB5yCY5LaA7NAk2lOGtCDBjuHeZBNfGY1tUPWcZCZBbQ8YA3x7OzbwBNIQkDILkmbXCSj61hNKHS74o9jZAWtx0OUvjJqK1gUMcKPeHsMU9zls1NaCCAjZBKM3FRCTQyavwtaK0qUaTaqTgPuzGzPhEWbFspK9VwDBaxrKdofiVL1YEqrqN2n24DdFGTr1ZAKvWMXxp4m4SKPn0cpH9k2wWTu8dyXfuOKqmSGXB52ftvnoBzsAHjlHlCZAXdMXSRB8iHvw0tuScDQZDZD";
+  const PAGE_ID = "632203696642033";
 
-    // 1. Converter para long-lived
-    const ltRes = await fetch(`https://graph.facebook.com/v25.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${APP_ID}&client_secret=${APP_SECRET}&fb_exchange_token=${SHORT}`);
-    const ltData = await ltRes.json();
-    if (!ltData.access_token) return res.status(400).json({ step: "long_lived_failed", error: ltData });
-    const LONG = ltData.access_token;
+  const r1 = await fetch(`https://graph.facebook.com/v25.0/${PAGE_ID}?fields=instagram_business_account,connected_instagram_account&access_token=${TOKEN}`);
+  const d1 = await r1.json();
 
-    // 2. Buscar paginas com long token
-    const pagesRes = await fetch(`https://graph.facebook.com/v25.0/me/accounts?fields=id,name,instagram_business_account,access_token&access_token=${LONG}`);
-    const pages = await pagesRes.json();
+  const r2 = await fetch(`https://graph.facebook.com/v25.0/me?fields=id,name,instagram_business_account&access_token=${TOKEN}`);
+  const d2 = await r2.json();
 
-    // 3. Para cada pagina buscar IG User ID
-    const results = [];
-    for (const page of (pages.data || [])) {
-      const igRes = await fetch(`https://graph.facebook.com/v25.0/${page.id}?fields=instagram_business_account,connected_instagram_account&access_token=${page.access_token}`);
-      const igData = await igRes.json();
-      results.push({
-        page: page.name,
-        page_id: page.id,
-        ig_user_id: igData.instagram_business_account?.id || igData.connected_instagram_account?.id || null
-      });
-    }
-
-    return res.status(200).json({ long_token: LONG, pages: results });
-  } catch(e) {
-    return res.status(500).json({ error: e.message });
-  }
+  return res.status(200).json({ page_fields: d1, me: d2 });
 }
