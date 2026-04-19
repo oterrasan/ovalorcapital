fd26fe412a346749b421aee0f7dceda1bd03febe
oterrasan <roberto@oterrasan.com.br>
2026-04-19 15:27:25 -0300

    token_setup: buscar IG ID OVC com page token

--- api/token_setup.js
+++ api/token_setup.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
-  const PAGE_ID = "1099986309860815";
-  const TOKEN = "EAAubUN64VCABRO7KTPUdneFSNaQuxkC1v4OdIDAyqFnGJNO7sZCdubyz81TaBpyIqWUx8W0nerZA11YLG6W3fo6bLFD16Ed7NC0Wkz7TDYoJBSYO2QiZBarjXJfosqTflAJJzXtLvq0baiI9kXzdszRK2I1wITIZB6yuySaA6faZAQ60dJRT8EhxHdUTIH2eXInPDCd8GK76L";
+  const TOKEN = "EAAubUN64VCABRKHaCJ1rH1wxZAB5yCY5LaA7NAk2lOGtCDBjuHeZBNfGY1tUPWcZCZBbQ8YA3x7OzbwBNIQkDILkmbXCSj61hNKHS74o9jZAWtx0OUvjJqK1gUMcKPeHsMU9zls1NaCCAjZBKM3FRCTQyavwtaK0qUaTaqTgPuzGzPhEWbFspK9VwDBaxrKdofiVL1YEqrqN2n24DdFGTr1ZAKvWMXxp4m4SKPn0cpH9k2wWTu8dyXfuOKqmSGXB52ftvnoBzsAHjlHlCZAXdMXSRB8iHvw0tuScDQZDZD";
+  const OVC_PAGE_ID = "1099986309860815";
 
-  // Buscar IG User ID da pagina O Valor Capital
-  const r1 = await fetch(`https://graph.facebook.com/v25.0/${PAGE_ID}?fields=instagram_business_account,connected_instagram_account,name&access_token=${TOKEN}`);
-  const d1 = await r1.json();
+  // Buscar IG direto da pagina O Valor Capital usando token de usuario
+  const r = await fetch(`https://graph.facebook.com/v25.0/${OVC_PAGE_ID}?fields=instagram_business_account,connected_instagram_account,name&access_token=${TOKEN}`);
+  const d = await r.json();
 
-  // Tambem buscar via /me/accounts para pegar page token
-  const r2 = await fetch(`https://graph.facebook.com/v25.0/me/accounts?fields=id,name,instagram_business_account,access_token&access_token=${TOKEN}`);
+  // Se nao veio, tentar buscar via me/accounts para pegar page access token da OVC
+  const r2 = await fetch(`https://graph.facebook.com/v25.0/me/accounts?fields=id,name,instagram_business_account,connected_instagram_account,access_token&access_token=${TOKEN}`);
   const d2 = await r2.json();
 
-  return res.status(200).json({ page_direct: d1, all_pages: d2 });
+  return res.status(200).json({ ovc_page: d, all_pages: d2.data });
 }
